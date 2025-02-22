4f0974ae236fc4358241066b7d965c19082bfb10
iichi <ismailichi29@gmail.com>
2025-02-22 14:25:42 +0100

    setting the base for SN project (frontend) + setting the handlers in progress...

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,14 +1,22 @@
 
 export default function LoginPage() {
     return (
-        <div className="login-container">
-            <h1>Login Page</h1>
-            <form method="post">
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <a href="/register">you don't have an account? click here</a>
-                <button className="btn"><a href="/home">Login</a></button>
-            </form>
-        </div>
+        <>
+            <div className="Container">
+                <div className="xx">
+                    <h1>Social Network</h1>
+                    <img width={200} height={200} src="images/SN-logo1.png" alt="logo" />
+                </div>
+                <div className="login-container">
+                    <h1>Login Page</h1>
+                    <form method="post">
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password" />
+                        <a href="/register">you don't have an account? click here</a>
+                        <button className="btn"><a href="/home">Login</a></button>
+                    </form>
+                </div>
+            </div>
+        </>
     );
 }
