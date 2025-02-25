a0856599fe3b858a12114bc5132768f069c0ea82
iichi <ismailichi29@gmail.com>
2025-02-25 11:00:03 +0000

    re-styling login/register containers

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,14 +1,14 @@
-
+import "./page.css";
 export default function LoginPage() {
     return (
         <>
             <div className="Container">
                 <div className="xx">
-                    <h1>Social Network</h1>
-                    <img width={200} height={200} src="images/SN-logo1.png" alt="logo" />
+                    <img width={150} height={150} src="images/SN-logo1.png" alt="logo" />
+                    <h2 style={{ color: "white", fontFamily: 'serif' }}>Login</h2>
                 </div>
                 <div className="login-container">
-                    <h1>Login Page</h1>
+                    <h1>Login</h1>
                     <form method="post">
                         <input type="text" placeholder="Username" />
                         <input type="password" placeholder="Password" />
--- frontend/app/register/page.js
+++ frontend/app/register/page.js
@@ -1,34 +1,41 @@
+
+import "./page.css"
+import Header from "../components/header";
 export default function LoginPage() {
     return (
-        <div className="login-container">
-            <h1>Register Page</h1>
-            <form method="post">
-                <div id="gender">
-                    <span>Gender</span>
-                    <input type="radio" name="gender" value="male" />
-                    <label htmlFor="male">Male</label>
-                    <input type="radio" name="gender" value="female" />
+        <>
+            {/* <Header /> */}
+            <div className="Container">
+                <div className="xx">
+                    <img width={170} height={170} src="images/SN-logo1.png" alt="logo" />
+                    <h2 style={{color: 'white', fontFamily:'serif'}}>Register</h2>
                 </div>
-                <input type="text" placeholder="username" />
-                <input type="text" placeholder="first name" />
-                <input type="text" placeholder="last name" />
-                <input type="number" placeholder="age" />
-                <input type="text" placeholder="Email" />
-                <span>Privacy :</span>
-                <select placeholder="Public/Private">
-                    <option value="public">Public</option>
-                    <option value="private">Private</option>
-                </select >
-                <input type="password" placeholder="Password" />
-                <span>(optional)</span>
-                <input type="file" datatype="image/*" placeholder="Avatar" />
-                <span>(optional)</span>
-                <input type="text" placeholder="Nickname" />
-                <span>(optional)</span>
-                <input type="text" placeholder="Bio" />
-                <a href="/login">you have an account? click here</a>
-                <button className="btn"><a href="/home" type="button">Register</a></button>
-            </form>
-        </div>
+                <div className="register-container">
+                    <form method="post">
+                        <div id="gender">
+                            <label htmlFor="Female">Female</label>
+                            <input type="radio" name="gender" value="male" />
+                            <label htmlFor="Male">Male</label>
+                            <input type="radio" name="gender" value="female" />
+                        </div>
+                        <input type="text" placeholder="username" />
+                        <input type="text" placeholder="first name" />
+                        <input type="text" placeholder="last name" />
+                        <input type="number" placeholder="age" />
+                        <input type="text" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <label >(optional)</label>
+                        <input type="file" datatype="image/*" placeholder="Avatar" />
+                        <label>(optional)</label>
+                        <input type="text" placeholder="Nickname" />
+                        <label>(optional)</label>
+                        <input type="text" placeholder="Bio" />
+                        <a href="/login">you have an account? click here</a>
+                        <button className="btn"><a href="/home" type="button">Register</a></button>
+                    </form>
+                </div>
+            </div>
+        </>
+
     );
 }
