a29b6ed00132b562b8736cf0c7412e2f280b8f4b
bbenskou <brahimbenskoura@gmail.com>
2025-03-12 21:23:02 +0000

    add check posts is exists

--- backend/pkg/reaction/insertReaction.go
+++ backend/pkg/reaction/insertReaction.go
@@ -2,13 +2,13 @@ package reaction
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"social-network/pkg/models"
 )
 
 // CreateReaction is a function that creates a reaction
 func InsertReaction(db *sql.DB, reaction *models.Reaction, userId int) error {
-
 	_, err := db.Exec("INSERT INTO reactions (user_id,post_id, group_id) VALUES (?,?, ?)", userId, reaction.PostID, reaction.GroupID)
 	if err != nil {
 		log.Println(err)
--- backend/pkg/reaction/reaction.go
+++ backend/pkg/reaction/reaction.go
@@ -3,6 +3,7 @@ package reaction
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"social-network/pkg/models"
@@ -18,14 +19,39 @@ func CreateReaction(w http.ResponseWriter, r *http.Request, db *sql.DB, userId i
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
+
+	err = checkReaction(db, &reaction, userId)
+	if err != nil {
+		log.Println(err)
+		utils.WriteJSON(w, http.StatusBadRequest, "bad request")
+		return
+	}
 	err = InsertReaction(db, &reaction, userId)
 	if err != nil {
 		log.Println(err)
 		utils.WriteJSON(w, http.StatusBadRequest, "bad request")
 		return
 	}
-	
-
 
 	w.WriteHeader(http.StatusCreated)
+}
+
+func checkReaction(db *sql.DB, reaction *models.Reaction, userId int) error {
+	if reaction.PostID <= 0 || reaction.GroupID < 0 {
+		return fmt.Errorf("PostID or GroupID is missing")
+	}
+	check := true
+	if reaction.GroupID != 0 {
+		db.QueryRow("SELECT EXISTS(SELECT id FROM posts WHERE id = ? AND group_id = ?)", reaction.PostID, reaction.GroupID).Scan(&check)
+		if !check {
+			return fmt.Errorf("Post does not exist")
+		}
+	} else {
+		db.QueryRow("SELECT EXISTS(SELECT id FROM posts WHERE id = ? AND group_id IS NULL)", reaction.PostID).Scan(&check)
+		if !check {
+			return fmt.Errorf("Post does not exist")
+		}
+	}
+
+	return nil
 }
