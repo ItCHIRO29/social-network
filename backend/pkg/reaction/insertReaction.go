d875b6f567725fd46ab74e4b4072d6b37cd9833c
bbenskou <brahimbenskoura@gmail.com>
2025-03-12 20:58:15 +0000

    fix err insert

--- backend/pkg/reaction/insertReaction.go
+++ backend/pkg/reaction/insertReaction.go
@@ -9,7 +9,7 @@ import (
 // CreateReaction is a function that creates a reaction
 func InsertReaction(db *sql.DB, reaction *models.Reaction, userId int) error {
 
-	_, err := db.Exec("INSERT INTO reactions (user_id,post_id, group_id) VALUES ($1, $2)", userId, reaction.PostID, reaction.GroupID)
+	_, err := db.Exec("INSERT INTO reactions (user_id,post_id, group_id) VALUES (?,?, ?)", userId, reaction.PostID, reaction.GroupID)
 	if err != nil {
 		log.Println(err)
 		return err
