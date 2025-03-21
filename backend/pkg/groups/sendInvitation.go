19c7482bebb9973eb86ef61613040985c1b697ed
iichi <ismailichi29@gmail.com>
2025-03-21 05:56:15 +0000

    adding members to the group (done)

--- backend/pkg/groups/sendInvitation.go
+++ backend/pkg/groups/sendInvitation.go
@@ -12,6 +12,7 @@ import (
 
 func SendInvitation(w http.ResponseWriter, r *http.Request, db *sql.DB, userId int) {
 	var Groups models.Groups
+	// var existingMembers bool
 	fmt.Println("Received request to send invitation")
 	// Decode the request body
 	err := json.NewDecoder(r.Body).Decode(&Groups)
@@ -20,22 +21,32 @@ func SendInvitation(w http.ResponseWriter, r *http.Request, db *sql.DB, userId i
 		utils.WriteJSON(w, 400, "Bad request: Invalid JSON")
 		return
 	}
-	fmt.Println("Received request to send invitation", Groups)
-	// Check if the user is already a member or invited
-	var existingID int
-	query := `SELECT user_id FROM group_members WHERE user_id = ? AND group_id = ?`
-	err = db.QueryRow(query, userId, Groups.Id).Scan(&existingID)
-
-	if err == nil {
-		fmt.Println("User is already a member or has a pending invitation.")
-		utils.WriteJSON(w, 400, "User already invited or in group")
-		return
-	} else if err != sql.ErrNoRows {
-		fmt.Println("Database error when checking existing invitation:", err)
+	fmt.Println("Received structure:", Groups)
+	existingMembers, err := CheckExistingInvitation(db, Groups.Invited_user_id, Groups.Id)
+	if err != nil {
+		fmt.Println("Error checking existing members:", err)
 		utils.WriteJSON(w, 500, "Internal server error")
 		return
 	}
+	if existingMembers {
+		fmt.Println("User already exists in the group")
+		utils.WriteJSON(w, 400, "User already exists in the group")
+		return
+	}
+	// if Groups.Invited_user_id != 0 && Groups.Invited_user_id != userId {
+	// 	fmt.Println("user ID ===>", Groups)
+	// 	// return
+	// }
 
+	if Groups.Invited_user_id != 0 && Groups.Invited_user_id != userId && !existingMembers {
+		_, err = db.Exec("INSERT INTO group_members (user_id, group_id, accepted) VALUES (?, ?, 0)", Groups.Invited_user_id, Groups.Id)
+		if err != nil {
+			fmt.Println("Error sending invitation:", err)
+			utils.WriteJSON(w, 500, "Internal server error")
+			return
+		}
+		return
+	}
 	// If user is not in the group, insert new invitation
 	_, err = db.Exec("INSERT INTO group_members (user_id, group_id, accepted) VALUES (?, ?, 0)", userId, Groups.Id)
 	if err != nil {
@@ -44,6 +55,24 @@ func SendInvitation(w http.ResponseWriter, r *http.Request, db *sql.DB, userId i
 		return
 	}
 
-	fmt.Println("Invitation sent successfully to user:", userId, "for group:", Groups.Id)
+	fmt.Println("Invitation sent successfully to user:", Groups.Invited_user_id, "for group:", Groups.Id)
 	utils.WriteJSON(w, 200, Groups)
+}
+
+func CheckExistingInvitation(db *sql.DB, userId int, groupId int) (bool, error) {
+	// Check if the user is already a member or invited
+	var existingID int
+	query := `SELECT user_id FROM group_members WHERE user_id = ? AND group_id = ?`
+	err := db.QueryRow(query, userId, groupId).Scan(&existingID)
+	if err == nil {
+		fmt.Println("userId that is in group :", existingID)
+		fmt.Println("userId that is in group :", userId)
+		fmt.Println("User is already a member or has a pending invitation.")
+		return true, err
+	} else if err != sql.ErrNoRows {
+		fmt.Println("Database error when checking existing invitation:", err)
+		// utils.WriteJSON(w, 500, "Internal server error")
+		return true, err
+	}
+	return false, nil
 }
--- frontend/app/components/groupsComponents/groupActivity/members.jsx
+++ frontend/app/components/groupsComponents/groupActivity/members.jsx
@@ -38,7 +38,7 @@ export default function Members({ groupData }) {
 function AddMembers({ groupId }) {
     console.log("groupId ===>", groupId);
     const [users, setUsers] = useState([]);
-    const [selectedUsers, setSelectedUsers] = useState([]);
+    // const [selectedUsers, setSelectedUsers] = useState([]);
     useEffect(() => {
         const fetchUsers = async () => {
             const data = await GetInviteMembers();
@@ -47,31 +47,29 @@ function AddMembers({ groupId }) {
         fetchUsers();
     }, []);
     // console.log("Group users to invit ===>", users);
-    console.log("selectedUsers ===>", selectedUsers);
+    // console.log("selectedUsers ===>", selectedUsers);
     return (
         <div id='add-members-list'>
             <button id="close" onClick={() => hide("add-members-list")}>X</button>
-            <form onSubmit={(e) => { InviteMembers(e, selectedUsers, groupId) }}>
+            <form >
                 {users ?
                     users.map((user) => (
-                        <div key={user.id} id='follower' onChange={
-                            (e) => {
-                                if (e.target.checked) {
-                                    setSelectedUsers(prevSelectedUsers => [...prevSelectedUsers, user.id]);
-                                }
-                            }
-                        }>
-                            <div id='invit-info'>
-                                <img src={`http://localhost:8080/` + user.image} alt="" />
-                                {user.full_name}
-                            </div>
-                            <input type="checkbox" value={user.id} />
-                        </div>
-                    )) : <h2>No users yet </h2>}
-                <button id="add-members" >
-                    <FontAwesomeIcon icon={faPlus} style={{ color: "#ffffff", fontSize: "20px" }} />
-                    Add Members
-                </button>
+                        <>
+                            <button key={user.id} id="follower" onClick={(e) => {
+                                InviteMembers(e, user.id, groupId)
+                                { console.log("user ===>", user) }
+
+                            }}>
+                                <div id='invit-info'>
+                                    <img src={`http://localhost:8080/` + user.image} alt="member" />
+                                    {user.full_name}
+                                </div>
+                                <FontAwesomeIcon icon={faPlus} style={{ backgroundColor: "green", color: "#ffffff", fontSize: "20px" }} />
+                            </button>
+                        </>
+                    )) : <p>No Uers To Add </p>
+                }
+
             </form>
         </div>
     );
@@ -100,29 +98,32 @@ async function GetInviteMembers() {
     }
 };
 
-async function InviteMembers(e, selectedUsers, groupId) {
+async function InviteMembers(e, userID, groupId) {
     e.preventDefault();
-    // let selectedMembers = [];
-    const formData = new FormData(e.target);
-    formData.append("invited_ids", JSON.stringify(selectedUsers || []));
-    formData.append("group_id", groupId);
-    console.log("formData", Object.fromEntries(formData));
+    console.log("group data ==> ", {
+        "group_id": groupId,
+        "invited_user_id": userID,
+    }
+    );
 
-    // const response = await fetch("http://localhost:8080/api/groups/invitation", {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //     },
-    //     credentials: "include",
-    //     body: JSON.stringify(Object.fromEntries(formData)),
-    // })
-    // if (!response.ok) {
-    //     alert("Failed to fetch users");
-    //     //router.push("/login");
-    //     //throw new Error("Failed to fetch users");
-    // }
-    e.target.reset();
-    console.log("selectedMembers ===>", selectedUsers);
+    const response = await fetch("http://localhost:8080/api/groups/invitation", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(
+            {
+                "id_group": groupId,
+                "invited_user_id": userID,
+            }
+        ),
+    })
+    if (!response.ok) {
+        alert("Failed to fetch users", response.status);
+        return;
+    }
+    // e.target.reset();
 }
 function Show(e, id) {
     const target1 = document.getElementById(id);
